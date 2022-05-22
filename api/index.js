80b931fa5ffd7f3a87926a775f473ad1ce609126
Fatima Ezzahrae Karaoui <fatimaezzahraekaraoui@gmail.com>
2022-05-22 16:15:10 +0100

    init

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
+import cookieParser from "cookie-parser"
+import cors from "cors"
 
 import authRoute from "./routes/auth.js"
 import usersRoute from "./routes/users.js"
@@ -28,9 +30,12 @@ mongoose.connection.on("disconnected" , ()=>{
 
 app.get("/" , (req,res) => {
     res.send("Hello first request !")
+    next()
 })
 
 //middelwares
+app.use(cors())
+app.use(cookieParser())
 app.use(express.json())
 
 app.use("/api/auth",authRoute);
@@ -38,7 +43,16 @@ app.use("/api/users",usersRoute);
 app.use("/api/hotels",hotelsRoute);
 app.use("/api/rooms",roomsRoute);
 
-
+app.use((err,req,res,next) =>{
+    const errorStatus=err.status || 500
+    const errorMessage=err.message || "Something went wrong !"
+    return res.status(errorStatus).json({
+        success:false,
+        status:errorStatus,
+        message:errorMessage,
+        stack:err.stack,
+    })
+})
 
 app.listen(9800, () => {
     connect()
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,18 +1,28 @@
 import express, { Router }  from "express";
-import Hotel from "../models/Hotel.js";
+import { 
+    countByCity,
+    countByType,
+    createHotel,
+    deleteHotel, 
+    getHotel, 
+    getHotels, 
+    updateHotel
+     } from "../controllers/hotelController.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/",async (res,req) =>{
-    const newHotel = new Hotel(req.body);
-    try{
-    const savedHotel = await newHotel.save();
-    res.status(200).json(savedHotel);
-    }catch(err){
-        res.status(500).json(err);
-    }
-});
+router.post("/",verifyAdmin, createHotel);
 //UPDATE
+router.put("/:id",verifyAdmin, updateHotel);
+//DELETE
+router.delete("/:id",verifyAdmin, deleteHotel);
+//GET
+router.get("/find/:id", getHotel);
+//GET ALL
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
 
 export default router
